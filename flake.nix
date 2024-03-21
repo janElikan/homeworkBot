03635794aaaaaa85b3e2bd2adf51cfe0d0a00811
Elikan <janelikan@proton.me>
2024-03-21 22:04:01 +0300

    feat: init flake from a template

--- flake.nix
+++ flake.nix
@@ -1,19 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils"; # cross-platform compatibility
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem # architectures
-    (system:
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
       in
-      with pkgs;
       {
-        devShells.default = mkShell {
-          buildInputs = [ cargo rustc rustfmt clippy bacon ];
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
-      }
-    );
+      });
 }
