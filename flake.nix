ec30fe9e677bb29fd3a67b43c13e0c6fefd19066
Elikan <janelikan@proton.me>
2024-04-02 20:41:21 +0300

    chore: build for x86-64 musl

--- flake.nix
+++ flake.nix
@@ -1,21 +1,43 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy bacon ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default =
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          target = "x86_64-unknown-linux-musl";
+          toolchain = with fenix.packages.${system}; combine [
+            minimal.cargo
+            minimal.rustc
+            targets.${target}.latest.rust-std
+          ];
+        in
+
+        (naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        }).buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ pkgsStatic.stdenv.cc ];
+          CARGO_BUILD_TARGET = target;
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
         };
-      });
+
+      devShell = with import nixpkgs { inherit system; }; mkShell {
+        buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy bacon ];
+        RUST_SRC_PATH = rustPlatform.rustLibSrc;
+      };
+    });
 }
